92fd05cb9f59d875c5d29c38c303bbb23fd7af09
lq92 <15157474420@163.com>
2018-03-29 20:43:53 +0800

    finish the ratings page but this page has the select all or positive or negative ratings bugs and now i haven't fix it

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,36 +1,57 @@
 <template>
-	<div class='ratings'>
-		<div class='section_top'>
-			<div class='comprehensive'>
-				<h2 class='score'>{{ seller.score }}</h2>
-				<h3 class='text'>综合评分</h3>
-				<p class='rank'>高于周边商家{{ seller.rankRate }}%</p>
-			</div>
-			<div class='score_container'>
-				<div class='service'>
-					<p class='text'>服务态度</p>
-					<div class='score_wrapper'>
-						<score :size='36' :score='seller.serviceScore'></score>
+	<div class='ratings' ref='ratings_wrapper'>
+		<div class='container_wrapper'>
+			<div class='section_top'>
+				<div class='comprehensive'>
+					<h2 class='score'>{{ seller.score }}</h2>
+					<h3 class='text'>综合评分</h3>
+					<p class='rank'>高于周边商家{{ seller.rankRate }}%</p>
+				</div>
+				<div class='score_container'>
+					<div class='service'>
+						<p class='text'>服务态度</p>
+						<div class='score_wrapper'>
+							<score :size='36' :score='seller.serviceScore'></score>
+						</div>
+						<span class='score_num'>{{ seller.serviceScore }}</span>
 					</div>
-					<span class='score_num'>{{ seller.serviceScore }}</span>
-				</div>
-				<div class='food'>
-					<p class='text'>商品评分</p>
-					<div class='score_wrapper'>
-						<score :size='36' :score='seller.foodScore'></score>
+					<div class='food'>
+						<p class='text'>商品评分</p>
+						<div class='score_wrapper'>
+							<score :size='36' :score='seller.foodScore'></score>
+						</div>
+						<span class='score_num'>{{ seller.foodScore }}</span>
 					</div>
-					<span class='score_num'>{{ seller.foodScore }}</span>
-				</div>
-				<div class='delivery'>
-					<p class='text'>送达时间</p>
-					<span class='time'>{{ seller.deliveryTime }}分钟</span>
+					<div class='delivery'>
+						<p class='text'>送达时间</p>
+						<span class='time'>{{ seller.deliveryTime }}分钟</span>
+					</div>
 				</div>
 			</div>
-		</div>
-		<gutter></gutter>
-		<div class='container'>
-			<select-btns :ratings='ratings'></select-btns>
-		</div>
+			<gutter></gutter>
+			<div class='container'>
+				<select-btns :ratings='ratings'></select-btns>
+				<div class='ratings_wrapper'>
+					<div class='ratings_item' v-for='item in ratings'>
+						<div class='avatar_wrapper'>
+							<img class='avatar' :src='item.avatar'>
+						</div>
+						<div class='rating'>
+							<h3 class='name'>{{ item.username }}</h3>
+							<div class='score_wrapper'>
+								<score :size='24' :score='item.score'></score>
+								<span class='delivery_time' v-if='item.deliveryTime'>{{ item.deliveryTime }}分钟送达</span>
+							</div>
+							<p class='text'>{{ item.text }}</p>
+							<div class='recommend_wrapper' v-if='item.recommend.length > 0'>
+								<i class='icon_thumb_up'></i>
+								<span class='recommend' v-for='recommend in item.recommend'>{{ recommend }}</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
 	</div>
 </template>
 
@@ -38,6 +59,7 @@
 import gutter from '~/gutter/gutter'
 import score from '~/score/score'
 import selectBtns from '~/select_btns/select_btns'
+import BScroll from 'better-scroll'
 export default {
 	data(){
 		return {
@@ -57,12 +79,16 @@ export default {
 		this.$http.get('/api/ratings').then(res => {
 			this.ratings = res.body.ratings
 		})
+		new BScroll(this.$refs.ratings_wrapper, {})
 	}
 }	
 </script>
 
 <style lang='sass' scoped>
+@import '../../assets/style.css'
 .ratings
+	height: 492px
+	overflow: hidden
 	.section_top
 		display: flex
 		padding: 18px 0
@@ -109,5 +135,65 @@ export default {
 				margin-bottom: 3px
 				.time
 					color: rgb(147, 153, 159)	
-	.container	
+	.container
+		.ratings_wrapper
+			.ratings_item
+				display: flex
+				padding: 18px 0
+				margin: 0 18px	
+				border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+				.avatar_wrapper
+					flex: 0 0 auto
+					margin-right: 11px
+					.avatar
+						width: 28px
+						height: 28px
+						border-radius: 50%
+				.rating
+					.name
+						font: 
+							size: 10px
+							weight: 400
+						color: rgb(7, 17, 27)	
+						margin-top: 1px 
+					.score_wrapper
+						display: flex
+						align-items: center
+						font-size: 10px
+						color: rgb(147, 153, 159)
+						margin: 6px 0 8px
+						.delivery_time
+							margin-left: 8px
+					.text
+						font: 
+							size: 12px
+							weight: 400
+						line-height: 18px
+						color: rgb(7, 17, 27)		
+						margin-bottom: 5px	
+					.recommend_wrapper
+						display: flex
+						align-items: center
+						flex-wrap: wrap
+						.icon_thumb_up
+							color: rgb(0, 160, 220)
+							font-size: 10px	
+							margin: 4px 8px 0 3px
+						.recommend
+							display: inline-block
+							width: 64px
+							height: 15px	
+							text-align: center
+							line-height: 15px
+							font: 
+								size: 9px
+								weight: 400
+							color: rgb(147, 153, 159)	
+							text-overflow: ellipsis
+							overflow: hidden
+							white-space: nowrap
+							border: 1px solid rgba(7, 17, 27, 0.1)
+							border-radius: 2px
+							margin-right: 8px
+							margin-top: 4px
 </style>
---
+++ src/components/select_btns/select_btns.vue
@@ -0,0 +1,103 @@
+<template>
+	<div class='select_btns'>
+		<div class='btns_wrapper'>
+			<div class='btn positive' :class='{ active: selectBtn }' @click='selectRatings'>{{ description.all }}<span class='num'>{{ ratings.length }}</span></div>
+			<div class='btn positive' :class='{ active: selectBtn }' @click='selectRatings'>{{ description.positive }}<span class='num'>{{ positive.length }}</span></div>
+			<div class='btn negative' :class='{ active: selectBtn }' @click='selectRatings'>{{ description.negative }}<span class='num'>{{ negative.length }}</span></div>
+		</div>
+		<div class='toggle_content'>
+			<i class='icon_check_circle'></i>
+			<p class='text'>只看有内容的评价</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		description: {
+			type: Object,
+			default: () => {
+				return {
+					all: '全部',
+					positive: '满意',
+					negative: '不满意'
+				}
+			}
+		}
+	},
+	data(){
+		return {
+			ratings: [],
+			positive: [],
+			negative: []
+		}
+	},
+	mounted(){
+		this.$http.get('/api/ratings').then(res => {
+			this.ratings = res.body.ratings;
+			this.positive = this.ratings.filter(item => {
+				return item.rateType === 0;
+			})
+			this.negative = this.ratings.filter(item => {
+				return item.rateType === 1;
+			})
+		})
+	},
+	computed: {
+		selectBtn(){
+			return true
+		}
+	},
+	methods: {
+		selectRatings(){
+
+		}
+	}
+}	
+</script>
+
+<style lang='sass' scoped>
+@import '../../assets/style.css'
+.select_btns
+	.btns_wrapper
+		display: flex
+		padding: 17px 0 18px
+		margin: 0 18px
+		border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+		.btn
+			margin-right: 8px
+			padding: 10px 12px
+			font: 
+				size: 12px
+				weight: 400
+			color: rgb(77, 85, 93)
+			.num
+				font-size: 8px
+				margin-left: 4px	
+			&:last-child
+				margin: 0
+		.positive, .negative
+			border-radius: 2px		
+		.positive
+			background: rgba(0, 160, 220, 0.2)
+		.negative
+			background: rgba(147, 153, 159, 0.2)		
+		.active
+			background: rgb(0, 160, 220)
+			color: #fff
+	.toggle_content
+		display: flex
+		align-items: center
+		height: 48px
+		border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+		.icon_check_circle
+			font-size: 20px
+			color: rgb(183, 187, 191)
+			margin: 0 6px 0 20px
+		.text
+			font: 
+				size: 12px
+				weight: 400
+			color: rgb(147, 153, 159)		
+</style>
