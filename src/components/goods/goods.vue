6573ee11c496c74b1888a3bf0b4c30c82d91a7a4
lq92 <15157474420@163.com>
2018-03-30 23:32:33 +0800

    init the page of goods

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,19 +1,42 @@
 <template>
 	<div class='goods'>
-		I'm goods
-		<score :size='36' :score='5'></score>
+		<div class='menu_wrapper' ref='menu_wrapper'>
+			<ul class='menu'>
+				<li class='item' v-for='item in goods'><span class='img'></span>{{ item.name }}</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
-import score from '~/score/score'
 export default {
-	components: {
-		score
+	data(){
+		return {
+			goods: []
+		}
+	},
+	mounted(){
+		this.$http.get('/api/goods').then(res => {
+			this.goods = res.body.goods
+		})
 	}
 }	
 </script>
 
 <style scoped lang='sass'>
-	
+.goods
+	.menu_wrapper
+		.menu
+			width: 80px
+			padding: 0 12px
+			background: rgb(243, 245, 247)
+			.item
+				width: 100%
+				height: 54px
+				box-sizing: border-box
+				font-size: 12px
+				color: rgb(7, 17, 27)
+				display: table
+				vertical-align: middle
+				border-bottom: 1px solid rgba(7, 17, 27, 0.1)
 </style>
